6f71daf92a17bc3138bf6bcea7c43f8d1417d91f
luthfeew <luthfeew@gmail.com>
2023-09-14 16:15:12 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Akun;
+use App\Livewire\Area;
+use App\Livewire\Beranda;
+use App\Livewire\Paket;
+use App\Livewire\Pelanggan;
+use App\Livewire\Pengguna;
+use App\Livewire\Tagihan;
+use App\Livewire\Transaksi;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +21,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', Beranda::class)->name('beranda');
+Route::get('/akun', Akun::class)->name('akun');
+Route::get('/area', Area::class)->name('area');
+Route::get('/paket', Paket::class)->name('paket');
+Route::get('/pelanggan', Pelanggan::class)->name('pelanggan');
+Route::get('/pengguna', Pengguna::class)->name('pengguna');
+Route::get('/tagihan', Tagihan::class)->name('tagihan');
+Route::get('/transaksi', Transaksi::class)->name('transaksi');
